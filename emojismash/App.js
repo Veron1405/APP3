45863f5e8844c20ed4200b41684a3dbfc4f9fcb3
Veron1405 <leonardo.verondias1405@gmail.com>
2024-04-29 16:50:10 -0300

    .

--- emojismash/App.js
+++ emojismash/App.js
@@ -1,9 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Platform } from "react-native";
 import Button from './components/Button';
 import * as ImagePicker from 'expo-image-picker';
 import ImageViewer from "./components/ImageViewer";
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
+import EmojiPicker from "./components/EmojiPicker";
+import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 
 const PlaceholderImage = require("./assets/images/background-image.png");
@@ -11,36 +20,135 @@ const PlaceholderImage = require("./assets/images/background-image.png");
 export default function App() {
 
   const [selectedImage, setSelectedImage] = useState(null);
-// const [showAppOptions, setShowAppOptions] = useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef();
 
-const pickImageAsync = async () => {
-  let result = await ImagePicker.launchImageLibraryAsync({
-    allowsEditing: true,
-    quality: 1,
-  });
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
 
-  if (!result.canceled) {
-    setSelectedImage(result.assets[0].uri);
-    // setShowAppOptions(true);
-  } else {
-    alert('You did not select any image.');
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  if (status === null) {
+    requestPermission();
   }
-};
+
+  // const onSaveImageAsync = async () => {
+  //   if (Platform.OS !== 'web') {
+  //     try {
+  //       const localUri = await captureRef(imageRef, {
+  //         height: 440,
+  //         quality: 1,
+  //       });
+  //       await MediaLibrary.saveToLibraryAsync(localUri);
+  //       if (localUri) {
+  //         alert('Saved!');
+  //       }
+  //     } catch (erro) {
+  //       console.log(erro);
+  //     }
+  //   } else {
+  //     try {
+  //       const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+  //         quality: 0.95,
+  //         width: 320,
+  //         height: 440,
+  //       });
+
+  //       let link = document.createElement('a');
+  //       link.download = 'sticker-smash.jpeg';
+  //       link.href = dataUrl;
+  //       link.click();
+  //     } catch (erro) {
+  //       console.log(erro);
+  //     }
+  //   }
+  // };
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
+      try {
+     
+        const localUri = await captureRef(imageRef, {
+          format: 'png',
+          quality: 1, 
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        alert('Saved!');
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        
+        const dataUrl = await domtoimage.toPng(imageRef.current, {
+          quality: 1,
+          width: 320,
+          height: 440,
+        });
+  
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.png'; 
+        link.href = dataUrl;
+        link.click();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer
-          placeholderImageSource={PlaceholderImage}
-          selectedImage={selectedImage}
-        />
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+          {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        </View>
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
-      </View>
-      <StatusBar style="auto"/>
-    </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset}  />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync}/>
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )
+      }
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
+
+
+      <StatusBar style="light" />
+    </GestureHandlerRootView>
 
   );
 }
@@ -58,5 +166,13 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
